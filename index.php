28962d653c32486f71cfcc084c93e25f485e6ce2
cocoarana <cocoarana@icloud.com>
2019-01-06 17:36:40 -0800

    Added new images to the respesctive folders

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Hugo's Mexican Kitchen | Main Page</title>
+    <link rel="icon" href="img/churroslogo.png">
+  </head>
+  <body>
+    <div class="" style="background-color:#000;">
+      <img src="img/Hugoslogo.png" alt="" height="500" width="500" style="transform:rotate(-90deg)">
+    </div>
+  </body>
+</html>
