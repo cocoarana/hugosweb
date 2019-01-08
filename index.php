3d53540a681e60e8ef083fb7fa4d73dfb2fd1824
cocoarana <cocoarana@icloud.com>
2019-01-07 21:42:31 -0800

    creation of a secondary logo plus started with the bone structure of the site, added top bar menu and css.

--- index.php
+++ index.php
@@ -4,10 +4,40 @@
     <meta charset="utf-8">
     <title>Hugo's Mexican Kitchen | Main Page</title>
     <link rel="icon" href="img/churroslogo.png">
+    <link rel="stylesheet" href="css/style.css">
   </head>
   <body>
-    <div class="" style="background-color:#000;">
-      <img src="img/Hugoslogo.png" alt="" height="500" width="500" style="transform:rotate(-90deg)">
+    <div class="header">
+      <img src="img/hugoslogo2.png" alt="Hugos MC Logo" id="logo">
+      <div class="navigation">
+        <div class="navbar">
+          <div class="dropdown">
+          <button class="dropbtn">Locations</button>
+            <div class="dropdown-content">
+              <a href="#">Surrey</a>
+              <a href="#">Chilliwack</a>
+              <a href="#">Richmond</a>
+            </div>
+          </div>
+          <a href="#">Gift Cards</a>
+          <a href="">Customer Feedback</a>
+          <a href="#">Reservations</a>
+        </div>
+        <div class="login">
+          <form class="loginform" action="index.html" method="post">
+            <input type="text" name="Username" placeholder="Username/Email">
+            <input type="password" name="Password" placeholder="Password">
+            <button type="submit" name="login">Sign In</button>
+          </form>
+          <a href="#">Sign Up</a>
+        </div>
+      </div>
+    </div>
+    <section>
+
+    </section>
+    <div class="footer">
+
     </div>
   </body>
 </html>
